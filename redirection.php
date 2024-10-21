5a89ffb2028f417e3c82e619d6228857513e60e0
MiftariB <48824614+MiftariB@users.noreply.github.com>
2024-10-21 11:18:22 +0200

    Create redirection.php

---
+++ redirection.php
@@ -0,0 +1,20 @@
+<?php
+
+/* Handy Code Provided by STEPHENCARR.NET */
+
+$iPod = stripos($_SERVER['HTTP_USER_AGENT'],"iPod");
+$iPhone = stripos($_SERVER['HTTP_USER_AGENT'],"iPhone");
+$iPad = stripos($_SERVER['HTTP_USER_AGENT'],"iPad");
+$Android= stripos($_SERVER['HTTP_USER_AGENT'],"Android");
+
+//check if user is using ipod, iphone or ipad...
+if( $iPod || $iPhone || $iPad ){
+        //we send these people to Apple Store
+        header('Location: https://apps.apple.com/be/app/cired-2024-chicago-workshop/id6633434464?l=fr-FR'); // <-apple store link here
+}else if($Android){
+        //we send these people to Android Store
+        header('Location: https://play.google.com/store/apps/details?id=com.crowdcomms.cired2024chicago&pli=1'); // <-android store link here
+}
+/* Handy Code Provided by STEPHENCARR.NET */
+
+?> 
